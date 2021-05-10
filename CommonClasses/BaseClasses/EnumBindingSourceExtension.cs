404a72327a232e4e332d42d8e5bc7fab31cb4cb7
Giuseppe Penna <penna_giuseppe@yahoo.it>
2021-05-10 18:08:31 +0200

    Cambiamento checkboxes con comboboxes

---
+++ CommonClasses/BaseClasses/EnumBindingSourceExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace CommonClasses.BaseClasses
+{
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        public Type EnumType { get; private set; }
+
+        public EnumBindingSourceExtension(Type type)
+        {
+            if (type is null || !type.IsEnum)
+                throw new Exception("type must be an Enum and not null!");
+            else
+                EnumType = type;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(EnumType);
+        }
+    }
+}
